d817e538aa11a6620c21343e36f719cc557c96dd
Jake Forrester <rannmann@rannmann.com>
2023-09-24 20:45:55 -0700

    feat: generate multiple cities at a time

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const zlib = require('zlib');
 const fs = require('fs');
 const path = require('path');
-const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const {Worker, isMainThread, parentPort, workerData} = require('worker_threads');
 const async = require('async');
 const os = require('os');
 
-//const SEATTLE_COORDINATES = {lat: 47.6097, lon: -122.3331};  // Seattle coordinates
-//const ANCHORAGE_AK_COORDINATES = {lat: 61.2181, lon: -149.9003};  // Anchorage, AK coordinates
-const BELLINGHAM_WA_COORDINATES = {lat: 48.7519, lon: -122.4787};  // Bellingham, WA coordinates
+// Reduces granularity of data by skipping every-other file.
+// This means the result set will represent every 10s instead of every 5s.
+// Speeds up data generation and reduces the output file size.
+const LIGHTWEIGHT_MODE = false;
 
-const SEARCH_COORDINATES = BELLINGHAM_WA_COORDINATES;
-let radii = [10, 20, 40, 60];  // Array of Radii in miles to generate multiple paths in one iteration.
+// Every city in here will have a flight path generated.
+// The more you have, the more RAM it uses, so be careful.
+const GENERATOR_COORDINATES = {
+    'GBR_London': {lat: 51.5074, lon: -0.1278},
+    'GBR_Manchester': {lat: 53.4808, lon: -2.2426},
+    'USA_AZ_Phoenix': {lat: 33.4484, lon: -112.0740},
+    'USA_CA_LosAngeles': {lat: 34.0522, lon: -118.2437},
+    'USA_CO_Denver': {lat: 39.7392, lon: -104.9903},
+    'USA_FL_Miami': {lat: 25.7617, lon: -80.1918},
+    'USA_GA_Atlanta': {lat: 33.7490, lon: -84.3880},
+    'USA_IL_Chicago': {lat: 41.8781, lon: -87.6298},
+    'USA_MA_Boston': {lat: 42.3601, lon: -71.0589},
+    'USA_MD_Baltimore': {lat: 39.2904, lon: -76.6122},
+    'USA_TX_Dallas': {lat: 32.7767, lon: -96.7970},
+    'USA_WA_Tacoma': {lat: 47.2529, lon: -122.4443},
+};
 
-// Object to hold flight paths
-let flightPaths = {};
-
-// Initialize flightPaths object for each radius
-radii.forEach(radius => {
-    flightPaths[radius] = {};
-});
+// Array of Radii in miles to generate multiple paths in one iteration.
+let radii = [20];
 
 /**
  * Function to calculate distance between two geographical coordinates.
@@ -56,39 +66,59 @@ function toRadians(degrees) {
 
 /**
  * Function to check if a geographical coordinate is within a given radius.
+ * Returns an object with a boolean for each city.
  *
  * @param lat
  * @param lon
  * @param radius
- * @returns {boolean}
+ * @returns {{}}
  */
 function isWithinRadius(lat, lon, radius) {
-    return distance(SEARCH_COORDINATES.lat, SEARCH_COORDINATES.lon, lat, lon) <= radius;
+    let result = {};
+
+    for (const city in GENERATOR_COORDINATES) {
+        result[city] = distance(GENERATOR_COORDINATES[city].lat, GENERATOR_COORDINATES[city].lon, lat, lon) <= radius;
+    }
+
+    return result;
 }
-if(isMainThread) {
+
+if (isMainThread) {
     async function main() {
-        const CONCURRENCY_LIMIT = os.cpus().length;
+        // JS multi-threading duplicates memory usage, so if this is too much, manually set it.
+        const CONCURRENCY_LIMIT = Math.round(os.cpus().length / 2);
         const directory = 'flight-history/2023-09-01';
         const files = fs.readdirSync(directory);
 
         let flightPaths = {};
 
-        radii.forEach(radius => {
-            flightPaths[radius] = {};
-        });
+        for (const city in GENERATOR_COORDINATES) {
+            flightPaths[city] = {};
+            radii.forEach(radius => {
+                flightPaths[city][radius] = {};
+            });
+        }
 
+        let i = 0;
         async.eachLimit(files, CONCURRENCY_LIMIT, (file, done) => {
+            if (LIGHTWEIGHT_MODE && i++ % 2 === 0) {
+                console.log(`Skipping ${file}`);
+                done();
+                return;
+            }
             processFile(path.join(directory, file))
                 .then(result => {
-                    radii.forEach(radius => {
-                        Object.keys(result[radius]).forEach(hex => {
-                            if(!flightPaths[radius][hex]) {
-                                flightPaths[radius][hex] = result[radius][hex];
-                            } else {
-                                flightPaths[radius][hex].geometry.coordinates.push(...result[radius][hex].geometry.coordinates);
-                            }
+                    for (const city in result) {
+                        radii.forEach(radius => {
+                            Object.keys(result[city][radius]).forEach(hex => {
+                                if (!flightPaths[city][radius][hex]) {
+                                    flightPaths[city][radius][hex] = result[city][radius][hex];
+                                } else {
+                                    flightPaths[city][radius][hex].geometry.coordinates.push(...result[city][radius][hex].geometry.coordinates);
+                                }
+                            });
                         });
-                    });
+                    }
 
                     console.log(`Processed ${file}`);
                     done();
@@ -98,16 +128,18 @@ if(isMainThread) {
                     done();
                 });
         }, err => {
-            if(err) console.error(err);
+            if (err) console.error(err);
 
-            radii.forEach(radius => {
-                const outFile = `./flightPaths_${radius}_miles.json`;
-                fs.writeFileSync(outFile, JSON.stringify({
-                    type: 'FeatureCollection',
-                    features: Object.values(flightPaths[radius]),
-                }));
-                console.log(`Flight paths for ${radius} miles radius written to ${outFile}`);
-            });
+            for (const city in flightPaths) {
+                radii.forEach(radius => {
+                    const outFile = `./flightpaths/${city}_${radius}_miles.json`;
+                    fs.writeFileSync(outFile, JSON.stringify({
+                        type: 'FeatureCollection',
+                        features: Object.values(flightPaths[city][radius]),
+                    }));
+                    console.log(`Flight paths for ${city} at ${radius} miles radius written to ${outFile}`);
+                });
+            }
         });
     }
 
@@ -118,9 +150,12 @@ if(isMainThread) {
     let file = workerData;
     let flightPaths = {};
 
-    radii.forEach(radius => {
-        flightPaths[radius] = {};
-    });
+    for (const city in GENERATOR_COORDINATES) {
+        flightPaths[city] = {};
+        radii.forEach(radius => {
+            flightPaths[city][radius] = {};
+        });
+    }
 
     const gunzip = zlib.createGunzip();
     const stream = fs.createReadStream(file).pipe(gunzip);
@@ -133,21 +168,27 @@ if(isMainThread) {
         // Process file for each radius
         radii.forEach(radius => {
             const data = JSON.parse(dataString);
-            data.aircraft
-                .filter(plane => isWithinRadius(plane.lat, plane.lon, radius))
-                .forEach(plane => {
-                    if (!flightPaths[radius][plane.hex]) {
-                        flightPaths[radius][plane.hex] = {
-                            type: 'Feature',
-                            geometry: {
-                                type: 'LineString',
-                                coordinates: []
-                            }
-                        };
+
+            data.aircraft.forEach(plane => {
+                let cityRadius = isWithinRadius(plane.lat, plane.lon, radius);
+
+                for (const city in cityRadius) {
+                    if (cityRadius[city]) {
+                        if (!flightPaths[city]) flightPaths[city] = {};
+                        if (!flightPaths[city][radius]) flightPaths[city][radius] = {};
+                        if (!flightPaths[city][radius][plane.hex]) {
+                            flightPaths[city][radius][plane.hex] = {
+                                type: 'Feature',
+                                geometry: { type: 'LineString', coordinates: [] }
+                            };
+                        }
+
+                        flightPaths[city][radius][plane.hex].geometry.coordinates.push([plane.lon, plane.lat]);
                     }
-                    flightPaths[radius][plane.hex].geometry.coordinates.push([plane.lon, plane.lat]);
-                });
+                }
+            });
         });
+
         parentPort.postMessage(flightPaths);
     });
 
@@ -164,7 +205,7 @@ function processFile(file) {
         worker.on('message', resolve);
         worker.on('error', reject);
         worker.on('exit', (code) => {
-            if(code !== 0) {
+            if (code !== 0) {
                 reject(new Error(`Worker stopped with exit code ${code}`));
             }
         })
