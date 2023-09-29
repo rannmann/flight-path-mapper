15beb13680fad9fc260097c859695d85a23631e1
Jake Forrester <rannmann@rannmann.com>
2023-09-28 21:31:49 -0700

    feat(heatmap): initial working rough draft

--- sound-heatmap.js
+++ sound-heatmap.js
@@ -1,69 +1,110 @@
-const AircraftLib = require('./lib/aircraft');
-const fs = require("fs");
+const asyncLib = require('async');
+const fs = require('fs');
 const zlib = require("zlib");
-const path = require("path");
+const {Worker, isMainThread, parentPort, workerData} = require('worker_threads');
+const AircraftLib = require('./lib/aircraft');
 const Geo = require('./lib/geo');
+const path = require("path");
+const os = require("os");
 
 const GENERATOR_COORDINATES = {
     'USA_WA_Seattle': {lat: 47.6062, lon: -122.3321},
 };
+const RADIUS_IN_MILES = 100;
 
-const RADIUS_IN_MILES = 50;
+if (isMainThread) {
+    // Main thread
+    const run = async () => {
+        const CONCURRENCY_LIMIT = Math.round(os.cpus().length / 1.8);
+        const directory = 'flight-history/2023-09-01';
+        const filesInDir = fs.readdirSync(directory);
+        //const targetFiles = filesInDir.slice(0, 600);
 
-// Our grid
-let grid = {};
+        let workerDataList = [];
 
-// Our aircraft library
-const aircraft = new AircraftLib(
-    GENERATOR_COORDINATES.USA_WA_Seattle.lat,
-    GENERATOR_COORDINATES.USA_WA_Seattle.lon
-);
+        await new Promise((resolve, reject) => {
+            asyncLib.eachLimit(filesInDir, CONCURRENCY_LIMIT, (file, cb) => {
+                new Worker(__filename, {workerData: path.join(directory, file)})
+                    .on('message', workerData => workerDataList.push(workerData))
+                    .on('exit', () => cb())
+                    .on('error', err => cb(err));
+            }, (err) => {
+                if (!err) resolve();
+                else reject(err);
+            });
+        });
 
-// Function to process all planes
-function processPlanes(planes) {
-    planes.forEach(plane => {
-        if (Geo.distanceInMiles(plane.lat, plane.lon, 47.6062, -122.3321) < RADIUS_IN_MILES) {
-            aircraft.addLoudnessToGrid(plane, grid);
-        }
-    });
-}
+        const allWorkerResults = workerDataList;
 
+        const aircraft = new AircraftLib(
+            GENERATOR_COORDINATES.USA_WA_Seattle.lat,
+            GENERATOR_COORDINATES.USA_WA_Seattle.lon
+        );
 
-// Todo: for now, just read the first 200 files
-async function processDataInFiles() {
-    const directory = 'flight-history/2023-09-01';
+        let grid = {};
 
-    const filesInDir = fs.readdirSync(directory);
-    const targetFiles = filesInDir.slice(0, 200);
-
-    for (const file of targetFiles) {
-        let dataString = '';
-        const gunzip = zlib.createGunzip();
-        const stream = fs.createReadStream(path.join(directory, file)).pipe(gunzip);
-
-        for await (const chunk of stream) {
-            dataString += chunk.toString();
-        }
+        // The grid being returned is only for 1 file.
+        // We need to add all the grids together to get the final grid.
+        allWorkerResults.forEach(workerGrid => {
+            for (let key in workerGrid) {
+                if (grid[key]) {
+                    // Combining the total loudness
+                    grid[key].totalLoudness += workerGrid[key].totalLoudness;
+                } else {
+                    // Making sure we are adding valid entries into the grid only
+                    if (workerGrid[key] && workerGrid[key].centralLat != null && workerGrid[key].centralLon != null) {
+                        grid[key] = workerGrid[key];
+                    }
+                }
+            }
+        });
 
         try {
-            const data = JSON.parse(dataString);
-            processPlanes(data.aircraft);
+            // Write grid to file
+            fs.writeFile('heatmap-grid.json', JSON.stringify(aircraft.convertGridBackToDegrees(grid)), (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("The file was saved!");
+                }
+            });
         } catch (err) {
-            console.error(`Failed to parse file ${file}:`, err);
+            console.error(err);
         }
-    }
-}
+    };
 
-async function main() {
-    await processDataInFiles().catch((err) => console.error(err));
-
-    // Write grid to file
-    fs.writeFile('heatmap-grid.json', JSON.stringify(aircraft.convertGridBackToDegrees(grid)), function(err) {
-        if(err) {
-            return console.log(err);
+    run().catch(console.error);
+} else {
+    // Worker
+    const processFile = async fileName => {
+        const gunzip = zlib.createGunzip();
+        const fileContent = fs.createReadStream(fileName).pipe(gunzip);
+        let dataString = '';
+        for await (const chunk of fileContent) {
+            dataString += chunk.toString();
         }
-        console.log("The file was saved!");
-    });
-}
+        const planes = JSON.parse(dataString).aircraft;
 
-main();
+        const aircraft = new AircraftLib(
+            GENERATOR_COORDINATES.USA_WA_Seattle.lat,
+            GENERATOR_COORDINATES.USA_WA_Seattle.lon
+        );
+
+        // Make a new grid
+        let grid = {};
+
+        // Process planes and generate grid
+        planes.forEach(plane => {
+            if (Geo.distanceInMiles(plane.lat, plane.lon, 47.6062, -122.3321) < RADIUS_IN_MILES) {
+                aircraft.addLoudnessToGrid(plane, grid);
+            }
+        });
+
+        // Return it for this file to be added to the main grid
+        return grid;
+    };
+
+    processFile(workerData).then((resultGrid) => {
+        parentPort.postMessage(resultGrid);
+    }).catch((error) => console.error(error));
+}
